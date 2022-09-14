118e2de68841458579f54e95c1e43895e16721d3
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2022-09-14 10:24:28 +0300

    added check for string

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ resultBtn.addEventListener('click', amountBit)
 function amountBit() {
   let getPrice = document.getElementById('get_price').value
   let getDollar = document.getElementById('get_dollar').value
+  let note = document.getElementsByClassName('note')
   let showResult = document.getElementById('show_result')
 
   if (!getPrice || !getDollar) {
@@ -12,9 +13,14 @@ function amountBit() {
     return
   }
 
-  showResult.innerHTML = (+getDollar / +getPrice).toFixed(7)
+  if (isNaN(+getPrice) || isNaN(+getDollar)) {
+    document.getElementById('get_price').value = ''
+    document.getElementById('get_dollar').value = ''
 
-  // clean the inputs:
-  //   document.getElementById('get_price').value = ''
-  //   document.getElementById('get_dollar').value = ''
+    showResult.innerHTML = 0
+    alert('Input must be a number!')
+    return
+  }
+
+  showResult.innerHTML = (+getDollar / +getPrice).toFixed(7)
 }
